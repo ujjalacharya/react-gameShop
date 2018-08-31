94875c6c0c385e96b25ac823028a77838b0dadb2
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-08-31 13:04:12 +0545

    adds checkbox

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+const tags = [
+  {_id: 1, name: "dice"},
+  {_id: 2, name: "economic"},
+  {_id: 3, name: "family"}
+];
+
 class GameForm extends React.Component {
   state = {
     title: "",
@@ -7,13 +13,25 @@ class GameForm extends React.Component {
     price: "",
     duration: "",
     players: "",
+    featured: false,
+    tags: []
   };
 
   handleChange = e => {
     //Changing to number if the type of input is number
-    let value = e.target.type === 'number' ? parseInt(e.target.value) : e.target.value
+    let value =
+      e.target.type === "number" ? parseInt(e.target.value) : e.target.value;
     this.setState({ [e.target.name]: value });
   };
+
+  handleCheckboxChange = e => {
+    this.setState({ [e.target.name]: e.target.checked });
+  };
+
+  toggleTag = tag => ()=>{
+    this.state.tags.includes(tag._id) ? this.setState({tags: this.state.tags.filter(id=> id !== tag._id)}):
+    this.setState({tags: [...this.state.tags, tag._id]})
+  }
 
   handleSubmit = e => {
     e.preventDefault();
@@ -33,6 +51,7 @@ class GameForm extends React.Component {
             placeholder="Title..."
           />
         </div>
+
         <div className="field">
           <label>Description</label>
           <textarea
@@ -43,6 +62,7 @@ class GameForm extends React.Component {
             placeholder="Description..."
           />
         </div>
+
         <div className="three fields">
           <div className="field">
             <label>Price(in cents)</label>
@@ -54,6 +74,7 @@ class GameForm extends React.Component {
               placeholder="Price..."
             />
           </div>
+
           <div className="field">
             <label>Duration(in mins)</label>
             <input
@@ -64,6 +85,7 @@ class GameForm extends React.Component {
               placeholder="Duration..."
             />
           </div>
+
           <div className="field">
             <label>Players</label>
             <input
@@ -74,6 +96,31 @@ class GameForm extends React.Component {
               placeholder="Players..."
             />
           </div>
+        </div>
+
+        <div className="inline field">
+          <label>Featured?</label>
+          <input
+            onChange={this.handleCheckboxChange}
+            checked={this.state.featured}
+            type="checkbox"
+            name="featured"
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="tags">Tags</label>
+          {tags.map(tag => (
+            <div key={tag._id} className="inline field">
+              <input
+                id={`tag=${tag._id}`}
+                onChange={this.toggleTag(tag)}
+                checked={this.state.tags.includes(tag._id)}
+                type="checkbox"
+              />
+              <label htmlFor={`tag-${tag._id}`}>{tag.name}</label>
+            </div>
+          ))}
         </div>
         <button className="ui button" type="submit">
           Submit
